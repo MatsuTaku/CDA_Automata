cc672da1ae3c26fc08f3f987d391525230bf4f27
MatsuTaku <tkm.matsumoto@gmail.com>
2019-05-20 11:36:21 +0900

    Refactoring on TailDict.

--- daram/include/csd_automata/TailDict.hpp
+++ daram/include/csd_automata/TailDict.hpp
@@ -25,17 +25,22 @@ class TailDictBuilder {
 private:
     const PlainFSA& orig_fsa_;
     
-    struct Container {
+    struct LabelUnit {
         id_type id = -1;
-        id_type owner_id = -1;
-        size_t pos = -1;
-        size_t counter = 0;
         std::string label = "";
         std::vector<size_t> nodes;
+        size_t counter = 0;
+        
+        id_type owner_id = -1;
+        size_t pos = -1;
+        
+        LabelUnit() = default;
+        LabelUnit(id_type id, std::string label, std::vector<size_t> nodes, size_t counter) : id(id), label(label), nodes(nodes), counter(counter) {}
+        
         bool is_merged() const {return owner_id != -1;}
         double priority() const {return double(counter) / label.size();}
     };
-    std::vector<Container> containers_;
+    std::vector<LabelUnit> label_container_;
     std::unordered_map<size_t, size_t> trans_to_label_id_;
     SerializedStringsBuilder serialized_strings_;
     
@@ -64,14 +69,14 @@ private:
         LabelArrange_(orig_fsa_.get_root_state(), std::make_shared<StringMap>(), std::make_shared<StateMap>());
         assert(malloc_zone_check(nullptr));
         
-        std::sort(containers_.begin(), containers_.end(), [](auto& lhs, auto& rhs) {
+        std::sort(label_container_.begin(), label_container_.end(), [](auto& lhs, auto& rhs) {
             return std::lexicographical_compare(lhs.label.rbegin(), lhs.label.rend(), rhs.label.rbegin(), rhs.label.rend());
         });
         
         {
-            Container dummy;
-            Container* prev = &dummy;
-            for (auto itr = containers_.rbegin(); itr != containers_.rend(); ++itr) {
+            LabelUnit dummy;
+            LabelUnit* prev = &dummy;
+            for (auto itr = label_container_.rbegin(); itr != label_container_.rend(); ++itr) {
                 auto& cur = *itr;
                 auto matches = 0;
                 auto owner_label_len = prev->label.length();
@@ -94,14 +99,14 @@ private:
         }
         assert(malloc_zone_check(nullptr));
         
-        std::sort(containers_.begin(), containers_.end(), [](auto& lhs, auto& rhs) {
+        std::sort(label_container_.begin(), label_container_.end(), [](auto& lhs, auto& rhs) {
             return (lhs.is_merged() != rhs.is_merged() ? lhs.is_merged() < rhs.is_merged() :
                     lhs.counter > rhs.counter);
         });
         std::unordered_map<id_type, size_t> owner_map;
         size_t i = 0;
-        for (; i < containers_.size() and not containers_[i].is_merged(); i++) {
-            auto& container = containers_[i];
+        for (; i < label_container_.size() and not label_container_[i].is_merged(); i++) {
+            auto& container = label_container_[i];
             auto index = serialized_strings_.size();
             container.pos = index;
             for (auto node : container.nodes) {
@@ -112,9 +117,9 @@ private:
             
             owner_map.insert({container.id, i});
         }
-        for (; i < containers_.size(); ++i) {
-            auto& container = containers_[i];
-            auto& owner = containers_[owner_map.at(container.owner_id)];
+        for (; i < label_container_.size(); ++i) {
+            auto& container = label_container_[i];
+            auto& owner = label_container_[owner_map.at(container.owner_id)];
             assert(owner.pos != -1);
             auto index = owner.pos + owner.label.size() - container.label.size();
             container.pos = index;
@@ -147,16 +152,11 @@ private:
                 auto cr = poplar::make_char_range(label_on_path);
                 auto* id_ptr = string_map->find(cr);
                 if (id_ptr == nullptr) {
-                    auto id = containers_.size();
+                    auto id = label_container_.size();
                     *(string_map->update(cr)) = id;
-                    Container container;
-                    container.id = id;
-                    container.label = label_on_path;
-                    container.nodes.push_back(index);
-                    container.counter = 1;
-                    containers_.push_back(container);
+                    label_container_.emplace_back(id, label_on_path, std::vector<size_t>{index}, 1);
                 } else {
-                    auto& container = containers_[*id_ptr];
+                    auto& container = label_container_[*id_ptr];
                     assert(container.id == *id_ptr);
                     container.nodes.push_back(index);
                     container.counter++;
